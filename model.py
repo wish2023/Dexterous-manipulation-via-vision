8b467d2dbbee648529dbe93fa261c994484f2896
Vishesh Arora <>
2024-12-06 00:09:43 -0500

    Add RGB model

--- model.py
+++ model.py
@@ -3,26 +3,35 @@ import torch
 import torch.nn as nn
 import torchvision.models as models
 
-
-
-class ResNet18RGB(nn.Module):
+class ResNet50RGB(nn.Module):
     def __init__(self, num_classes=5):
-        super(ResNet18RGB, self).__init__()
+        super(ResNet50RGB, self).__init__()
         
-        self.model = models.resnet18(weights='DEFAULT')
+        self.model = models.resnet50(weights='DEFAULT')
         self.model.fc = nn.Linear(self.model.fc.in_features, num_classes)
 
     def forward(self, x):
         return self.model(x)
 
 
-class ResNet18RGBD(nn.Module):
+class VGG16RGB(nn.Module):
     def __init__(self, num_classes=5):
-        super(ResNet18RGBD, self).__init__()
+        super(VGG16RGB, self).__init__()
         
-        self.model = models.resnet18(weights='DEFAULT')
+        self.model = models.vgg16(weights='DEFAULT')
+        self.model.classifier[6] = nn.Linear(self.model.classifier[6].in_features, num_classes)
+
+    def forward(self, x):
+        return self.model(x)
+
+
+class ResNet50RGBD(nn.Module):
+    def __init__(self, num_classes=5):
+        super(ResNet50RGBD, self).__init__()
         
-        # Modify first con layer
+        self.model = models.resnet50(weights='DEFAULT')
+        
+        # Modify the first convolutional layer
         rgb_conv1 = self.model.conv1
         self.model.conv1 = nn.Conv2d(
             in_channels=4,  # RGB + Depth
@@ -33,9 +42,11 @@ class ResNet18RGBD(nn.Module):
             bias=rgb_conv1.bias
         )
         
+        # Initialize new weights for the depth channel
         with torch.no_grad():
             self.model.conv1.weight[:, :3, :, :] = rgb_conv1.weight  # Copy RGB weights
 
+        # Modify the final fully connected layer for the number of classes
         self.model.fc = nn.Linear(self.model.fc.in_features, num_classes)
 
     def forward(self, x):
@@ -44,7 +55,7 @@ class ResNet18RGBD(nn.Module):
 
 
 class DepthAutoencoder(nn.Module):
-    def __init__(self, latent_dim=128):
+    def __init__(self, latent_dim=512):
         super(DepthAutoencoder, self).__init__()
         
         # Encoder
@@ -53,22 +64,22 @@ class DepthAutoencoder(nn.Module):
             nn.ReLU(),
             nn.Conv2d(32, 64, kernel_size=3, stride=2, padding=1), # 32x112x112 -> 64x56x56
             nn.ReLU(),
-            nn.Conv2d(64, 128, kernel_size=3, stride=2, padding=1), # 64x56x56 -> 128x28x28
+            nn.Conv2d(64, latent_dim, kernel_size=3, stride=2, padding=1), # 64x56x56 -> latent_dimx28x28
             nn.ReLU(),
             nn.Flatten(),
-            nn.Linear(128 * 28 * 28, latent_dim)
+            nn.Linear(latent_dim * 28 * 28, latent_dim)  # Latent representation
         )
         
         # Decoder
         self.decoder = nn.Sequential(
-            nn.Linear(latent_dim, 128 * 28 * 28),
-            nn.Unflatten(1, (128, 28, 28)),
-            nn.ConvTranspose2d(128, 64, kernel_size=3, stride=2, padding=1, output_padding=1),
+            nn.Linear(latent_dim, latent_dim * 28 * 28),
+            nn.Unflatten(1, (latent_dim, 28, 28)),
+            nn.ConvTranspose2d(latent_dim, 64, kernel_size=3, stride=2, padding=1, output_padding=1),
             nn.ReLU(),
             nn.ConvTranspose2d(64, 32, kernel_size=3, stride=2, padding=1, output_padding=1),
             nn.ReLU(),
             nn.ConvTranspose2d(32, 1, kernel_size=3, stride=2, padding=1, output_padding=1),
-            nn.Sigmoid() # Consider removing sigmoid
+            nn.Sigmoid()  # To scale output between 0 and 1
         )
 
     def forward(self, x):
@@ -80,14 +91,14 @@ class DepthAutoencoder(nn.Module):
 
 
 class ResNetDepthFusion(nn.Module):
-    def __init__(self, autoencoder_weights='autoencoder.pt', resnet_weights='DEFAULT', latent_dim=128, num_classes=5):
+    def __init__(self, autoencoder_weights='autoencoder.pt', resnet_weights='DEFAULT', latent_dim=512, num_classes=5):
         super(ResNetDepthFusion, self).__init__()
 
         self.resnet = models.resnet50(weights=resnet_weights)
-        self.resnet = torch.nn.Sequential(*(list(self.resnet.children())[:-1]))
         self.fc = nn.Linear(self.resnet.fc.in_features + latent_dim, num_classes)
+        self.resnet = torch.nn.Sequential(*(list(self.resnet.children())[:-1]))
         self.depth_autoencoder = DepthAutoencoder(latent_dim=latent_dim)
-        self.depth_autoencoder.load_state_dict(torch.load(autoencoder_weights, weights_only=True))
+        self.depth_autoencoder.load_state_dict(torch.load(autoencoder_weights, map_location=torch.device('cpu')))
 
     def forward(self, rgb, depth):
         output1 = self.resnet(rgb).squeeze(-1).squeeze(-1)
